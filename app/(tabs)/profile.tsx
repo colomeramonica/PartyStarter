3ffc42a2562d809a74ca8e0cf6d263291fee3449
Mônica Colomera <colomeramonica@gmail.com>
2024-11-20 07:35:53 -0300

    (refactor): Profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarBadge, AvatarFallbackText, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallbackText, AvatarImage } from "@/components/ui/avatar";
 import { Box } from "@/components/ui/box";
 import { Button, ButtonText } from "@/components/ui/button";
 import { Center } from "@/components/ui/center";
@@ -14,60 +14,27 @@ import { VStack } from "@/components/ui/vstack";
 import { useCallback, useState } from "react";
 import { ScrollView } from 'react-native';
 
-const char = {
-  name: "Jester Lavorre",
-  class: "Cleric",
-  race: "Tiefling",
-  alignment: "Chaotic Neutral",
-  description: "Jester Lavorre is a 5 feet, 3 inches tall blue tiefling with blue hair. She wears a pretty dress and has freckles. She wears a belt with a symbol to the Traveler on her waist. Her voice makes her sound younger than she is.",
-  affiliation: "Mighty Nein",
-}
+interface Character {
+  name: string;
+  class: string;
+  race: string;
+  alignment: string;
+  description: string;
+  affiliation: string;
+};
 
-const [character, setCharacter] = useState(char);
-
-const handleChange = useCallback((field: string, value: string) => {
-  setCharacter((prevCharacter) => ({
-    ...prevCharacter,
-    [field]: value,
-  }));
-}, []);
-
-const handleUpload = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-  const file = event.target.files?.[0];
-  // if (file) {
-  //   const storageRef  = ref(storage, `images/${file.name}`);
-  //   const uploadTask = uploadBytesResumable(storageRef, file);
-  //   uploadTask.on(
-  //     "state_changed",
-  //     (snapshot: any) => {
-  //       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-  //       console.log("Upload is " + progress + "% done");
-  //     },
-  //     (error: any) => {
-  //       console.error("Upload error:", error);
-  //     },
-  //     () => {
-  //       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL: string) => {
-  //         console.log('File available at', downloadURL);
-  //       setImage(downloadURL); 
-  //       });
-  //     }
-  //   );
-  // }
-}, []);
-
-const CharacterAvatar = () => (
+const CharacterAvatar = ({ character }: { character: Character }) => (
   <Avatar size="xl">
     <AvatarFallbackText>{character.name}</AvatarFallbackText>
     <AvatarImage
       source={{
-        uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        uri: require('../../assets/images/Jester.jpg'),
       }}
     />
   </Avatar>
 );
 
-const CharacterForm = () => (
+const CharacterForm = ({ character, handleChange }: { character: Character, handleChange: (field: string, value: string) => void }) => (
   <VStack className="p-4 rounded-md w-full">
     <FormControl
       isInvalid={false}
@@ -164,36 +131,84 @@ const CharacterForm = () => (
   </VStack>
 );
 
-const PhotoGallery = () => (
+const PhotoGallery = ({ handleUpload }: { handleUpload: (event: React.ChangeEvent<HTMLInputElement>) => void }) => (
   <Box className="flex items-center justify-center">
     <Heading className="font-poppins mt-3 text-xl">Photo Gallery</Heading>
     <HStack space="md" reversed={false} className="duration-300 hover:opacity-75 mt-2 transition-opacity">
-      <input type="file" onChange={handleUpload} style={{ display: 'none' }} name="photos" />
-      <Image
-        size="md"
-        source={{
-          uri: "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-        }}
-        alt="image"
-        className="rounded-md"
+      <label htmlFor="upload-photo">
+        <Image
+          size="md"
+          source={{
+            uri: "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+          }}
+          alt="image"
+          className="cursor-pointer rounded-md"
+        />
+      </label>
+      <input
+        id="upload-photo"
+        type="file"
+        onChange={handleUpload}
+        style={{ display: 'none' }}
+        name="photos"
       />
     </HStack>
   </Box>
 );
 
 export default function Profile() {
+  const [character, setCharacter] = useState<Character>({
+    name: "Jester Lavorre",
+    class: "Cleric",
+    race: "Tiefling",
+    alignment: "Chaotic good",
+    description: "Jester Lavorre is a blue tiefling with blue hair. She has a mischievous smile and a penchant for pranks. She is a cleric of the Traveler, a deity of chaos and trickery. Jester is a member of the Mighty Nein, a group of adventurers who travel the world in search of treasure and glory.",
+    affiliation: "",
+  });
+
+  const handleChange = useCallback((field: string, value: string) => {
+    setCharacter((prevCharacter: Character) => ({
+      ...prevCharacter,
+      [field]: value,
+    }));
+  }, []);
+
+  const handleUpload = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    // const file = event.target.files?.[0];
+    // if (file) {
+    //   const storageRef  = ref(storage, `images/${file.name}`);
+    //   const uploadTask = uploadBytesResumable(storageRef, file);
+    //   uploadTask.on(
+    //     "state_changed",
+    //     (snapshot: any) => {
+    //       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    //       console.log("Upload is " + progress + "% done");
+    //     },
+    //     (error: any) => {
+    //       console.error("Upload error:", error);
+    //     },
+    //     () => {
+    //       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL: string) => {
+    //         console.log('File available at', downloadURL);
+    //       setImage(downloadURL);
+    //       });
+    //     }
+    //   );
+    // }
+  }, []);
+
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
       <Center className="h-full justify-start p-3 w-full">
         <Box className="bg-white flex items-center mt-1 p-3 rounded-xl w-full">
-          <Box className="flex justify-start">
-            <CharacterAvatar />
+          <CharacterAvatar character={character} />
+          <Box className="flex justify-start w-full">
+            <CharacterForm character={character} handleChange={handleChange} />
+            <PhotoGallery handleUpload={handleUpload} />
           </Box>
-          <CharacterForm />
-          <PhotoGallery />
         </Box>
       </Center>
-      <Center className="bg-white flex flex-row justify-end px-3 rounded-xl">
+      <Center className="flex flex-row h-full justify-end p-3 rounded-xl w-full">
         <Button size="md" variant="solid" action="primary">
           <ButtonText>Save</ButtonText>
         </Button>
